e1e27af9a19b61ab02d4c1edb9e7ec7c74ba9e97
Manuel Chang <changmanuel14@gmail.com>
2020-09-23 16:40:56 -0600

    se agrego la plantilla base se agrego la vista en detalle de las publicaciones

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,13 @@
 from django.utils import timezone
 from django.shortcuts import render
 from .models import Publicacion
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def publicacion_lista(request):
     publicaciones = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/publicacion_lista.html', {'publicaciones': publicaciones})
+
+def publicacion_detalle(request, pk):
+    publicacion = get_object_or_404(Publicacion, pk=pk)
+    return render(request, 'blog/publicacion_detalle.html', {'publicacion': publicacion})
